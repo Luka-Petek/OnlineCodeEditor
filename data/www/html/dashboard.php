ae27f9137a7f1a9b0d67f52dea2fe7f25f1613e7
DaDudee <luka.petek45@gmail.com>
2025-11-16 16:58:32 +0100

    shranjevanje in fetchanje programov za vse projekte

--- data/www/html/dashboard.php
+++ data/www/html/dashboard.php
@@ -23,26 +23,61 @@ if (!$projekt) {
     exit;
 }
 
-$jezik = strtolower($projekt['jezik'] ?? 'default');
-$zacetna_vsebina_raw = $projekt['vsebina'] ?? '';
+// *** LOGIKA ZA NALAGANJE SHRANJENE KODE ***
+require_once 'php/db.php'; 
+$db = getDatabaseConnection(); 
+
+$jezik = strtolower($projekt['jezik'] ?? 'default'); 
+$zacetna_vsebina_raw = '';
+
+$ekstenzija = match ($jezik) {
+    'java' => 'java', 'python' => 'py', 'c' => 'c', 
+    'javascript' => 'js', 'html' => 'html', 
+    default => 'txt',
+};
+$imeDatoteke = "main." . $ekstenzija; 
+
+$sql_check_file = "SELECT potDatoteke FROM datoteka WHERE FKprojekt = :projekt_id AND imeDatoteke = :imeDatoteke";
+$stmt_check_file = $db->prepare($sql_check_file);
+$stmt_check_file->bindParam(':projekt_id', $projekt_id, PDO::PARAM_INT);
+$stmt_check_file->bindParam(':imeDatoteke', $imeDatoteke);
+$stmt_check_file->execute();
+$shranjena_pot = $stmt_check_file->fetchColumn();
+
+
+if ($shranjena_pot) {
+    if (file_exists($shranjena_pot)) {
+        $vsebina_iz_diska = file_get_contents($shranjena_pot);
+        
+        if ($vsebina_iz_diska !== false) {
+            $zacetna_vsebina_raw = $vsebina_iz_diska;
+        } 
+        else {
+            error_log("Napaka pri branju datoteke: " . $shranjena_pot);
+        }
+    } 
+    else {
+        error_log("Datoteka ni najdena na disku, čeprav je zapisana v bazi: " . $shranjena_pot);
+    }
+}
 
 if (empty(trim($zacetna_vsebina_raw))) {
     switch ($jezik) {
         case 'java':
-            $zacetna_vsebina_raw = "public class Main {\n    public static void main(String[] args) {\n        System.out.println(\"Pozdravljen, Java!\");\n    }\n}";
+            $zacetna_vsebina_raw = "public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Pozdravljen, Java!\");\n\t}\n}";
             break;
         case 'python':
-            $zacetna_vsebina_raw = "def main():\n    print(\"Pozdravljen, Python!\")\n\nif __name__ == \"__main__\":\n    main()";
+            $zacetna_vsebina_raw = "def main():\n\tprint(\"Pozdravljen, Python!\")\n\nif __name__ == \"__main__\":\n\tmain()";
             break;
         case 'c':
-            $zacetna_vsebina_raw = "#include <stdio.h>\n\nint main() {\n    printf(\"Pozdravljen, C!\\n\");\n    return 0;\n}";
+            $zacetna_vsebina_raw = "#include <stdio.h>\n\nint main() {\n\tprintf(\"Pozdravljen, C!\\n\");\n\treturn 0;\n}";
             break;
         case 'javascript':
             $zacetna_vsebina_raw = "console.log('Pozdravljen, JavaScript!');";
             break;
         case 'html':
             $project_title = htmlspecialchars($projekt['imeProjekta'] ?? 'Projekt');
-            $zacetna_vsebina_raw = "<!DOCTYPE html>\n<html>\n<head>\n    <title>" . $project_title . "</title>\n</head>\n<body>\n    <h1>Pozdravljen, HTML!</h1>\n</body>\n</html>";
+            $zacetna_vsebina_raw = "<!DOCTYPE html>\n<html>\n<head>\n\t<title>" . $project_title . "</title>\n</head>\n<body>\n\t<h1>Pozdravljen, HTML!</h1>\n</body>\n</html>";
             break;
         default:
             $zacetna_vsebina_raw = "// Jezik ni prepoznan. Vnesite svojo kodo.";
@@ -50,7 +85,9 @@ if (empty(trim($zacetna_vsebina_raw))) {
 }
 $zacetna_vsebina = htmlspecialchars($zacetna_vsebina_raw);
 
-$codemirror_mode = 'clike';
+$codemirror_mode = 'clike'; 
+$mode_script = 'mode/clike/clike.min.js';
+
 switch ($jezik) {
     case 'python':
         $codemirror_mode = 'python';
@@ -61,7 +98,7 @@ switch ($jezik) {
         $mode_script = 'mode/javascript/javascript.min.js';
         break;
     case 'html':
-        $codemirror_mode = 'xml';
+        $codemirror_mode = 'xml'; 
         $mode_script = 'mode/xml/xml.min.js';
         break;
     case 'java':
@@ -73,7 +110,6 @@ switch ($jezik) {
         $mode_script = 'mode/clike/clike.min.js';
         break;
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -129,6 +165,7 @@ Pritisnite "Zaženi Kodo" za simulacijo izvedbe.
             </pre>
         </div>
     </main>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/codemirror.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.15/<?php echo $mode_script; ?>"></script>
     <?php if ($jezik === 'html'): ?>
@@ -170,8 +207,7 @@ Prikazana je koda za <?php echo strtoupper($projekt['jezik'] ?? 'NEZNANO'); ?>.
             }, 800);
         });
 
-        //SHRANJEVANJE DATOTEKE
-
+        // *** SHRANI BUTTON LOGIKA (AJAX) ***
         const PROJEKT_ID = <?php echo json_encode($projekt_id); ?>;
         const JEZIK = '<?php echo $jezik; ?>';
         const SAVE_URL = 'php/shraniKodo.php';
---
+++ data/www/user_files/1/9/main.js
@@ -0,0 +1,3 @@
+console.log('Pozdravljen, JavaScript!');
+console.log('testiranje shranjevanja');
+console.log('testiranje komplet shranjevanja in dodajanja')
